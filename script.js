440b97f871de3b4915949fdd00aa6d6a223a8e0d
Bodiu Crinu <bodiucrinu@gmail.com>
2025-04-09 12:48:52 +0200

    updateeeeee

--- script.js
+++ script.js
@@ -7,4 +7,19 @@ function calculerSalaire() {
 
     const ass = salaire * 0.07;
     document.getElementById('ass').value = ass.toFixed(2);
+
+    var pen = salaire * 0.05;
+    document.getElementById('pen').value = pen.toFixed(2);
+
+    if (document.getElementById(form).checked) {
+        total += 150
+    }
+       
+    if (document.getElementsById("100K").checked) {
+        total += 150
+    }
+    
+    var total = salaire - impot - ass + pen;
+
+    document.getElementById('salNet').value = total.toFixed(2);
 }
